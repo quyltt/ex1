5d8dab4a7a2d1d68c6edff5abdc94b0843f35480
Thanh Quy <thanhquylethi@gmail.com>
2023-07-15 16:30:34 +0700

    Add project

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,61 @@
+package main;
+
+import com.google.common.base.Joiner;
+
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // BTVN Day1
+        int n;
+        Scanner sc = new Scanner(System.in);
+        // create new array
+        System.out.print("Enter the number of elements you want to store: ");
+        n = sc.nextInt();
+
+        int[] arrInt = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arrInt[i] = sc.nextInt();
+        }
+
+        int[] arrOutput = addArrayWith1(arrInt);
+        System.out.println("Result: ");
+        for (int i = 0; i < arrOutput.length; i++) {
+            System.out.print(arrOutput[i]);
+        }
+    }
+
+    public static int[] addArrayWith1(int[] arrInput) {
+
+        // convert array to string
+        String stringNumber = Joiner.on("").join(Collections.singleton(arrInput));
+
+        // Convert string to int
+        int intNumber = Integer.parseInt(stringNumber);
+
+        // Plus 1
+        intNumber++;
+
+        // Convert int to sting
+        String stringResult = Integer.toString(intNumber);
+
+        // Get first zero in array
+        int numberZeroFirst = arrInput.length - stringResult.length();
+        StringBuilder zeroFirst = new StringBuilder();
+        if (numberZeroFirst > 0) {
+            zeroFirst.append("0".repeat(numberZeroFirst));
+            stringResult = zeroFirst + stringResult;
+        }
+
+        // Convert to int array and Output result
+        int[] intArrResult = new int[stringResult.length()];
+
+        System.out.println("Result: ");
+        for (int i = 0; i < intArrResult.length; i++) {
+            intArrResult[i] = Character.getNumericValue(stringResult.charAt(i));
+        }
+        return intArrResult;
+    }
+}
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+    @Test
+    void tc01() {
+        int[] arr = {8, 9, 8};
+    }
+
+}
